c8efaea15221ead339f493e414b03bc8fa1d397e
adrianoaraujo0 <adriano.amedeiros@hotmail.com>
2025-02-06 20:43:38 -0300

    fix CORS

--- src/main/java/com/livraria/api/config/CorsConfig.java
+++ src/main/java/com/livraria/api/config/CorsConfig.java
@@ -3,10 +3,12 @@ package com.livraria.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
 
 	
     @Bean
