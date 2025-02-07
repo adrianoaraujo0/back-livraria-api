fea64bc290fb698be8888f6cc7d33d2425c91758
adrianoaraujo0 <adriano.amedeiros@hotmail.com>
2025-02-07 15:52:16 -0300

    remove end point test

--- src/main/java/com/livraria/api/controller/UserController.java
+++ src/main/java/com/livraria/api/controller/UserController.java
@@ -33,15 +33,6 @@ public class UserController {
 		this.userService = userService;
 	}
 	
-	@Operation(summary = "EndPoint Teste")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Testar API.")
-	})
-	@GetMapping("hello")
-	public ResponseEntity<Object> helloWorld(){
-		return ResponseEntity.status(HttpStatus.OK).body("HELLO WORLD!!!!!!!!");
-	}
-	
 	@Operation(summary = "Pegar todos os usuários")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Usuário carregados com sucesso.")
