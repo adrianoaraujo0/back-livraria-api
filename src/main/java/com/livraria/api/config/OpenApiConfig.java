4811e3f5afea102bd0e5646e131b36ac3cd5ec24
adrianoaraujo0 <adriano.amedeiros@hotmail.com>
2025-02-07 12:41:22 -0300

    fix CORS

--- src/main/java/com/livraria/api/config/OpenApiConfig.java
+++ src/main/java/com/livraria/api/config/OpenApiConfig.java
@@ -12,7 +12,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-        		.addServersItem(new Server().url("http://api.minhalivraria.com"))
+        		.addServersItem(new Server().url("https://back-livraria-api-production.up.railway.app"))
                 .addServersItem(new Server().url("http://localhost:8080"));
     }
 }
