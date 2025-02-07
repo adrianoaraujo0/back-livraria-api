818f8b32c56a1ac37200544ee4582c7a8aed4101
adrianoaraujo0 <adriano.amedeiros@hotmail.com>
2025-02-06 22:47:29 -0300

    fix CORS

--- src/main/java/com/livraria/api/config/CorsConfig.java
+++ src/main/java/com/livraria/api/config/CorsConfig.java
@@ -9,12 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer{
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOriginPatterns("*") 
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
-                .allowedHeaders("*") 
-                .allowCredentials(true); 
-    }
+	 @Override
+     public void addCorsMappings(CorsRegistry registry) {
+		 registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+     }
 }
